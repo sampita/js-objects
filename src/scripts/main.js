63d947fb88da3e16598d0c7f1ea1a5c9b5c0abec
Sam Pita <samanthakpita@gmail.com>
2019-10-09 15:12:17 -0500

    next lightning exercises

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -41,3 +41,21 @@ const son = {
 
 const familyMembers = [mom, dad, daughter, son]
 console.log(familyMembers);
+
+
+
+// Lightning Exercise 4: Given the object below, output each of the values to the console using dot notation.
+
+const wardrobe = {
+    height: 80,
+    manufacturer: "Killibrew & Sons",
+    contents: ["Dress shirt", "Jeans", "Suit", "Skirt", "Tennis shoes"],
+    depth: 38,
+    width: 50
+}
+
+console.log(`The wardrobe is ${wardrobe.height} inches tall`);
+console.log(`The wardrobe is made by ${wardrobe.manufacturer}`);
+console.log(`The wardrobe is full of ${wardrobe.contents[0]}, ${wardrobe.contents[1]}, ${wardrobe.contents[2]}, ${wardrobe.contents[3]}, ${wardrobe.contents[4]}`);
+console.log(`The wardrobe is ${wardrobe.depth} inches deep`);
+console.log(`The wardrobe is ${wardrobe.width} inches wide`);
