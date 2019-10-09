88ad860e6d90d9fa4239fa93fe0f66d588b4cf4c
Sam Pita <samanthakpita@gmail.com>
2019-10-09 14:43:46 -0500

    completed lightning exercises for Book 2 Chapter 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,43 @@
-console.log("We're learning about objects!");
+// Testing if js is linked to html
+// console.log("We're learning about objects!");
+
+// Lightning Exercise 1
+const car = {
+    year: 2015,
+    brand: "Ford",
+    model: "Mustang",
+    color: "red"
+}
+
+// Lightning Exercise 2
+const animals = ["Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky"]
+
+
+// Lightning Exercise 3
+const mom = {
+    name: "Maria",
+    age: 52,
+    role: "mother"
+}
+
+const dad = {
+    name: "Armando",
+    age: 55,
+    role: "father"
+}
+
+const daughter = {
+    name: "Nicole",
+    age: 29,
+    role: "daughter"
+}
+
+const son = {
+    name: "Nicholas",
+    age: 21,
+    role: "son"
+}
+
+
+const familyMembers = [mom, dad, daughter, son]
+console.log(familyMembers);
