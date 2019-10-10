7ed4ff98f57a286ae608b3b8eba10ba4a6392754
Sam Pita <samanthakpita@gmail.com>
2019-10-10 10:23:28 -0500

    lightning exercise four

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,11 +9,10 @@ const car = {
     color: "red"
 }
 
-// Lightning Exercise 2
+
 const animals = ["Kippers", "Jack", "Gypsy", "Angus", "Seymour Bouts", "Sharky"]
 
 
-// Lightning Exercise 3
 const mom = {
     name: "Maria",
     age: 52,
@@ -44,7 +43,7 @@ console.log(familyMembers);
 
 
 
-// Lightning Exercise 4: Given the object below, output each of the values to the console using dot notation.
+// Lightning Exercise 2: Given the object below, output each of the values to the console using dot notation.
 
 const wardrobe = {
     height: 80,
@@ -60,7 +59,7 @@ console.log(`The wardrobe is full of ${wardrobe.contents[0]}, ${wardrobe.content
 console.log(`The wardrobe is ${wardrobe.depth} inches deep`);
 console.log(`The wardrobe is ${wardrobe.width} inches wide`);
 
-//Lightning Exercise 5:
+//Lightning Exercise 3:
 
 const empireStateBuilding = {
     stories: 103,
@@ -77,7 +76,7 @@ const empireStateBuilding = {
 
 console.log(`The empire state building is ${empireStateBuilding.height} high with ${empireStateBuilding.stories} stories. The east west length is ${empireStateBuilding.eastWestLength} and the north south length is ${empireStateBuilding.northSouthLength}. It is ${empireStateBuilding.squareFeet} square feet big.`)
 
-// Lightning Exercise 6:
+
 const addressKey = "address"
 const constructionKey = "constructionDate"
 const ownerKey = "owner"
@@ -85,3 +84,36 @@ const costKey = "cost"
 const architectKey = "architect"
 
 console.log(`The empire state building is at ${empireStateBuilding[addressKey]}. It was built in ${empireStateBuilding[constructionKey]} by ${empireStateBuilding[architectKey]}. It cost ${empireStateBuilding[costKey]} and is owned by ${empireStateBuilding}.`)
+
+
+
+// Lightning Exercise 4
+
+const nashvilleSoftwareSchool = {
+    founded: 2012,
+    director: "John Wark",
+    instructors: {
+        fullTime: ["Jisie", "Brenda", "Steve", "Joe", "Andy"],
+        partTime: ["Zoe", "Nathan"]
+    },
+    address: "500 Interstate Blvd. S"
+}
+
+// question 1: Output the names of the part-time instructors followed by the names of the full-time instructors in the console.
+
+const ptInstructors = nashvilleSoftwareSchool.instructors.partTime
+
+const ftInstructors = nashvilleSoftwareSchool.instructors.fullTime
+
+for (let i = 0; i < ptInstructors.length; i++) {
+    console.log(ptInstructors[i])
+}
+
+for (let i = 0; i < ftInstructors.length; i++) {
+    console.log(ftInstructors[i])
+}
+
+// question 2: Output only Andy and Zoe in the console.
+
+
+console.log(ftInstructors[4], ptInstructors[0])
